df669c52235e2fb2ac8f9b7a8021dc57ad57cfe3
Benjamin Caspari <becaspari@gmail.com>
2013-05-07 11:03:17 +0200

    show german mpaa flags

--- alter.sh
+++ alter.sh
@@ -12,3 +12,18 @@ perlregex() {
 
 # enable germany mpaa setting 
 perlregex '720p/SkinSettings.xml' 's|<!--(item[^>]*id="3".*31702.*?)/item-->|<\1/item>|'
+
+# choose right flag for german mpaa ratings
+if ! grep -q 'fsk-18' '720p/IncludesVariables.xml' ; then
+	perlregex '720p/IncludesVariables.xml' \
+	's|(\000\s*)(<variable name="rating">).*?\000|\1\2'\
+	'\1\t<value condition="stringcompare(Skin.String(MPAACountryCert),$LOCALIZE[31702]) + substring(listitem.mpaa,18)">de/fsk-18</value>'\
+	'\1\t<value condition="stringcompare(Skin.String(MPAACountryCert),$LOCALIZE[31702]) + substring(listitem.mpaa,16)">de/fsk-16</value>'\
+	'\1\t<value condition="stringcompare(Skin.String(MPAACountryCert),$LOCALIZE[31702]) + substring(listitem.mpaa,12)">de/fsk-12</value>'\
+	'\1\t<value condition="stringcompare(Skin.String(MPAACountryCert),$LOCALIZE[31702]) + substring(listitem.mpaa,6)">de/fsk-6</value>'\
+	'\1\t<value condition="stringcompare(Skin.String(MPAACountryCert),$LOCALIZE[31702]) + substring(listitem.mpaa,0)">de/fsk-0</value>'\
+	'\1\t<value condition="stringcompare(Skin.String(MPAACountryCert),$LOCALIZE[31702]) + substring(listitem.mpaa,o)">de/fsk-0</value>'\
+	'\n\n|' 
+fi
+
+#cat 720p/IncludesVariables.xml | tr '\n' '\0' | ssed -R "$R" | tr '\0' '\n' >720p/IncludesVariables.xml2	
