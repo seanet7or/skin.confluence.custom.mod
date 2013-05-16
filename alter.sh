455fd6a81f0d30048a1b8dbc76270178eb387a05
Benjamin Caspari <becaspari@gmail.com>
2013-05-16 20:49:48 +0200

    supress error messages

--- alter.sh
+++ alter.sh
@@ -68,13 +68,13 @@ fi
 # remove Home menu seperator
 if grep -q 'HomeSeperator' '720p/Home.xml' ; then
 	perlregex '720p/Home.xml' 's|\000\s*?<control type="image">\s*?\000\s*?<posx>[0-9]+</posx>\s*?\000\s*?<posy>[0-9]+</posy>\s*?\000\s*?<width>[0-9]+</width>\s*?\000\s*?<height>[0-9]+</height>\s*?\000\s*?<texture>HomeSeperator.png</texture>\s*?\000\s*?</control>\s*?||g'
-	rm 'media/HomeSeperator.png'
+	rm 'media/HomeSeperator.png' 2>/dev/null
 fi
 
 # remove Home overlay
 if grep -q 'HomeOverlay1.png' '720p/Home.xml' ; then
 	perlregex '720p/Home.xml' 's|\000\s*?<control type="image">\s*?\000\s*?<posx>[0-9]+</posx>\s*?\000\s*?<posy>[0-9]+</posy>\s*?\000\s*?<width>[0-9]+</width>\s*?\000\s*?<height>[0-9]+</height>\s*?\000\s*?<texture>HomeOverlay1.png</texture>\s*?\000\s*?</control>\s*?||g'
-	rm 'media/HomeOverlay1.png'
+	rm 'media/HomeOverlay1.png' 2>/dev/null
 fi
 
 # remove menu items seperator
@@ -111,7 +111,7 @@ if grep -q 'RecentAddedBack' '720p/IncludesHomeWidget.xml' ; then
 '\s*?<height>[0-9]+</height>\s*?\000'\
 '\s*?<texture[^>]*>RecentAddedBack.png</texture>\s*?\000'\
 '\s*?</control>\s*?||g'
-	rm media/RecentAddedBack.png
+	rm media/RecentAddedBack.png 2>/dev/null
 fi
 
 # remove widgets titel label
