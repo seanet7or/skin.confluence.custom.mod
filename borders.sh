7199d23d7ac6ed5fec07f89d2f0c2999e371f993
Benjamin Caspari <becaspari@gmail.com>
2013-06-25 19:25:07 +0200

    Removed gradient.png

--- borders.sh
+++ borders.sh
@@ -1,71 +1,20 @@
+source textures.sh
 
-TEXLIST='button-focus2.png;2
-icon-mute.png;0
-OverlayWatched.png;0
-DefaultIconError.png;0
-Fanart_Fallback_Music_Small.jpg;0
-black-back.png;0
-StackNF.png;3
-StackFO.png;3
-DefaultFile.png;0
-DefaultIconWarning;0
-DefaultIconInfo;0
-button-focus_light.png;2
-folder-focus_light.png;2
-button-focus.png;2
-black-back2.png;0
-KeyboardKey.png;4
-KeyboardEditArea_light.png;0
-MediaBladeSub_light.png;7,0,7,0
-HomeBladeSub_light.png;7,0,7,0
-MenuItemFO_light.png;2
-HomeSubNF_light.png;0,2,0,0
-HomeSubFO.png;2
-HomeSubEnd.png;0,2,0,2
-floor_button.png;5
-floor_buttonFO.png;5
-StackFO.png;5
-\$INFO\[ListItem.Property\(Icon\)\];0
-\$INFO\[ListItem.Property\(Addon.StarRating\),LeftRating/\];0
-\$INFO\[ListItem.Art\(fanart\)\];0
-\$INFO\[ListItem\.Icon\];0
-OSDProgressMidLight_light.png;0
-OSDProgressMid_light.png;0
-OSDProgressBack_light.png;0
-\$INFO\[Container\(50\).Listitem.Icon\];0
-epg-genres/\$INFO\[ListItem.Property\(GenreType\)\].png;3
-\$INFO\[Listitem.Icon\];0
-NoCover_1.png;4
-flagging/blank.png;4
-HomeBack.png;0,6,0,6
-scroll-down-2.png;12
-scroll-up-2.png;12
-scroll-down-focus-2.png;12
-scroll-up-focus-2.png;12
-scroll-right-focus.png;0
-scroll-left-focus.png;0
-scroll-right.png;0
-scroll-left.png;0
-osd_slider_nib.png;0
-osd_slider_nibNF.png;0
-osd_slider_bg.png;0
-radiobutton-nofocus.png;0
-scroll-down-focus.png;0
-scroll-down.png;0
-scroll-up-focus.png;0
-scroll-up.png;0
-KeyboardKeyNF.png;1'
 OLDIFS=$IFS ; IFS=$'\n'
 for T in $TEXLIST ; do
+	if [ -z "$T" ] ; then
+		continue
+	fi
 	TEXTURE=$(echo "$T" | cut -f1 -d';')
 	BORDER=$(echo "$T" |cut -f2 -d';')
-	ARGS+='|'$TEXTURE
+	ARGS+="|$TEXTURE"
+	continue
 	if [ "$BORDER" == "0" ] ; then
 		if grep ">$TEXTURE" 720p/*.xml 2>/dev/null | grep -q 'border="' ; then
 			echo "ERROR: $TEXTURE"
 			grep ">$TEXTURE" 720p/*.xml 2>/dev/null | grep 'border="'	| head -n 1	
 		fi
-		else
+	else
 		if grep ">$TEXTURE" 720p/*.xml 2>/dev/null | grep -v -q 'border="'$BORDER'"' ; then
 			echo "ERROR: $TEXTURE"
 			grep ">$TEXTURE" 720p/*.xml 2>/dev/null | grep -v 'border="'$BORDER'"'	| head -n 1	
@@ -74,6 +23,7 @@ for T in $TEXLIST ; do
 done
 
 ARGS=$(echo "$ARGS" | sed 's/^|//')
+echo "$ARGS"
 REMOVED='>DialogBack.png|>DialogContextBottom.png|>DialogContextMiddle.png|>DialogContextTop.png|'
 REMOVED+='>InfoMessagePanel.png|'
 REMOVED+='>KeyboardEditArea.png|>KeyboardCornerTop.png|>KeyboardCornerTopNF.png|>KeyboardCornerBottom.png|>KeyboardCornerBottomNF.png'
--- modify.sh
+++ modify.sh
@@ -188,9 +188,10 @@ check_and_remove() {
 		if ! grep -I -q "[^a-z_\.\(]$BASE[^a-zA-Z0-9 _\.=/]" 720p/* ; then
 			rm "$FILE" # 2>/dev/null
 		else
-			printf "\n'$BASE' ('$FILE') was found in the .xmls: \n"
+			printf "\n'$BASE' ('$FILE') was found in the .xmls (deleting anyway): \n"
 			grep "[^a-z_\.\(]$BASE[^a-zA-Z0-9 _\.=/]" 720p/*
 			#exit 3
+			rm "$FILE" # 2>/dev/null
 		fi
 	else
 		printf "\n'$FILENAME' ('$FILE') was found in the .xmls: \n"
@@ -1096,10 +1097,20 @@ fi
 step
 
 printf "\nChanging title bars: "
-if true ; then #[ -f media/GlassTitleBar.png ] ; then
+if [ -f media/GlassTitleBar.png ] ; then
 	remove_control image '<texture>GlassTitleBar.png</texture>'
 	remove_control image '<texture flipy="true">GlassTitleBar.png</texture>'
 	check_and_remove media/GlassTitleBar.png
+	printf "%sDONE!%s" $GREEN $RESET
+else
+	printf "%sSKIPPED.%s" $CYAN $RESET
+fi
+step
+
+printf "\nRemoving gradient.png: "
+if [ -f media/gradient.png ] ; then
+	remove_control image '<texture>gradient.png</texture>'
+	>/dev/null check_and_remove media/gradient.png
 	printf "%sDONE!%s" $GREEN $RESET
 else
 	printf "%sSKIPPED.%s" $CYAN $RESET
@@ -1339,73 +1350,21 @@ else
 fi
 step
 
-TEXLIST='button-focus2.png;2
-icon-mute.png;0
-OverlayWatched.png;0
-DefaultIconError.png;0
-Fanart_Fallback_Music_Small.jpg;0
-black-back.png;0
-StackNF.png;3
-StackFO.png;3
-DefaultFile.png;0
-DefaultIconWarning;0
-DefaultIconInfo;0
-button-focus_light.png;2
-folder-focus_light.png;2
-button-focus.png;2
-black-back2.png;0
-KeyboardKey.png;4
-KeyboardEditArea_light.png;0
-MediaBladeSub_light.png;7,0,7,0
-HomeBladeSub_light.png;7,0,7,0
-MenuItemFO_light.png;2
-HomeSubNF_light.png;0,2,0,0
-HomeSubFO.png;2
-HomeSubEnd.png;0,2,0,2
-floor_button.png;5
-floor_buttonFO.png;5
-StackFO.png;5
-\$INFO\[ListItem.Property\(Icon\)\];0
-\$INFO\[ListItem.Property\(Addon.StarRating\),LeftRating/\];0
-\$INFO\[ListItem.Art\(fanart\)\];0
-\$INFO\[ListItem\.Icon\];0
-OSDProgressMidLight_light.png;0
-OSDProgressMid_light.png;0
-OSDProgressBack_light.png;0
-\$INFO\[Container\(50\).Listitem.Icon\];0
-epg-genres/\$INFO\[ListItem.Property\(GenreType\)\].png;3
-\$INFO\[Listitem.Icon\];0
-NoCover_1.png;4
-flagging/blank.png;4
-HomeBack.png;0,6,0,6
-scroll-down-2.png;12
-scroll-up-2.png;12
-scroll-down-focus-2.png;12
-scroll-up-focus-2.png;12
-scroll-right-focus.png;0
-scroll-left-focus.png;0
-scroll-right.png;0
-scroll-left.png;0
-osd_slider_nib.png;0
-osd_slider_nibNF.png;0
-osd_slider_bg.png;0
-radiobutton-nofocus.png;0
-scroll-down-focus.png;0
-scroll-down.png;0
-scroll-up-focus.png;0
-scroll-up.png;0
-KeyboardKeyNF.png;1'
+source textures.sh
 OLDIFS=$IFS ; IFS=$'\n'
 for T in $TEXLIST ; do
+	if [ -z "$T" ] ; then
+		continue
+	fi
 	TEXTURE=$(echo "$T" | cut -f1 -d';')
 	BORDER=$(echo "$T" |cut -f2 -d';')
 	printf "\nSetting correct borders for $TEXTURE: "
 	if ! [ "$BORDER" == "0" ] ; then
 		if grep ">$TEXTURE" 720p/* | grep -v -q 'border="'$BORDER'"' ; then
 			XMLS=$(2>/dev/null grep "$TEXTURE" -l 720p/*)
-			R='s|border="[0-9,]*"(\| flipx="true")>'$TEXTURE'</|border="'$BORDER'"\1>'$TEXTURE'</|g'
+			R='s|border="[0-9,]*"(\| flipx="true")>('$TEXTURE')</|border="'$BORDER'"\1>\2</|g'
 			perlregex $XMLS "$R" --nocheck
-			R='s|([a-z])>'$TEXTURE'</|\1 border="'$BORDER'">'$TEXTURE'</|g'
+			R='s|([a-z])>('$TEXTURE')</|\1 border="'$BORDER'">\2</|g'
 			perlregex $XMLS "$R" --nocheck
 			printf "%sDONE!%s" $GREEN $RESET
 		else
@@ -1414,7 +1373,7 @@ for T in $TEXLIST ; do
 	else
 		if grep ">$TEXTURE" 720p/* | grep -q 'border="' ; then
 			XMLS=$(2>/dev/null grep "$TEXTURE" -l 720p/*)
-			R='s| border="[0-9,]*">'$TEXTURE'</|>'$TEXTURE'</|g'
+			R='s| border="[0-9,]*">('$TEXTURE')</|>\1</|g'
 			perlregex $XMLS "$R"
 			printf "%sDONE!%s" $GREEN $RESET
 		else
